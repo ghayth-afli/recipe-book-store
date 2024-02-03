de70b12f6aec9c807c0bc648c40e88ac34c101d3
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-02-03 14:36:02 +0100

    adding a shopping List Edit Section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 20),
+  ];
 }
