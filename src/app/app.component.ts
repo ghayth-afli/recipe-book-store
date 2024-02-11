8cc98fa8958cd7c624990f188a786d069a813bf0
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-02-11 21:54:52 +0100

    adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'recipe-book-store';
+  shopList: boolean = false;
+  recipes: boolean = true;
+  navigateTo(msg) {
+    this.shopList = msg.shopList;
+    this.recipes = msg.recipes;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  @Output() msgToParent = new EventEmitter<{
+    recipes: boolean;
+    shopList: boolean;
+  }>();
 
+  toRecipes() {
+    this.msgToParent.emit({
+      recipes: true,
+      shopList: false,
+    });
+  }
+  toShoppingList() {
+    this.msgToParent.emit({
+      recipes: false,
+      shopList: true,
+    });
+  }
 }
